964d60070faee27e03b7ddc2899c949d97e4777b
vagrant <vagrant@HADES>
2018-06-27 12:25:14 +0000

    Add test coverage

--- etl/database.py
+++ etl/database.py
@@ -13,22 +13,22 @@ class Database(object):
         self.logger = logger or logging.getLogger(__name__)
         self.connection = None
 
-    def set_connection(self, server_name, database_name, user_name, password):
+    def set_connection(self, server_name, port, database_name, user_name, password):
         """
         Get database connection
         """
-        self.connection = psycopg2.connect(host=server_name, dbname=database_name,
+        self.connection = psycopg2.connect(host=server_name, port=port, dbname=database_name,
                                 user=user_name, password=password)
         return self.connection
 
-    def import_file(self, connection, file_path, table_name, delimiter='|', header=True):
+    def import_file(self, file_path, table_name, delimiter='|', header=True):
         """
         Import text file to database
         """
         sql = 'COPY {0} FROM STDIN WITH(FORMAT CSV, DELIMITER \'{1}\', \
               HEADER {2})'.format(table_name, delimiter, header)
         local_file = open(file_path)
-        cursor = connection.cursor()
+        cursor = self.connection.cursor()
         cursor.copy_expert(sql, local_file)
-        connection.commit()
+        self.connection.commit()
         cursor.close()
---
+++ tests/test_database.py
@@ -0,0 +1,67 @@
+import unittest
+import shutil
+import os
+import uuid
+import psycopg2
+import testing.postgresql
+
+from sqlalchemy import create_engine
+
+from context import etl
+
+
+class TestDatabase(unittest.TestCase):
+    """
+    Testing Database class
+    """
+
+    def test_set_connection(self):
+        """
+        Test set connection
+        """
+        with testing.postgresql.Postgresql() as postgresql:
+            create_engine(postgresql.url())
+            dsn = postgresql.dsn()
+            database = etl.database.Database()
+            connection = database.set_connection(dsn['host'], dsn['port'], dsn['database'], dsn['user'], None)
+            self.assertEqual(str(type(connection)), '<class \'psycopg2.extensions.connection\'>')
+
+    def test_importing_a_text_file(self):
+        """
+        Test imprting file to database
+        """
+        with testing.postgresql.Postgresql() as postgresql:
+            create_engine(postgresql.url())
+            dsn = postgresql.dsn()
+            database = etl.database.Database()
+            database.connection = psycopg2.connect(**dsn)
+            cursor = database.connection.cursor()
+            cursor.execute('create table test_table (col1 varchar(20), col2 varchar(20));')
+
+            # create test directory in tmp folder
+            root_path = '/tmp/'
+            directory_name = str(uuid.uuid4())
+            directory_path = os.path.join(root_path, directory_name)
+            os.mkdir(directory_path)
+
+            # create test file in test directory
+            test_file_path = os.path.join(directory_path, 'test.file')
+            f = open(test_file_path, 'w')
+            f.write('col1|col2\n')
+            f.write('test1|test2')
+            f.close()
+
+            # test the function
+            database.import_file(test_file_path, 'test_table')
+            cursor.execute('select * from test_table;')
+            row = cursor.fetchone()
+
+            # clean up
+            shutil.rmtree(directory_path)
+
+            self.assertEqual(row[0], 'test1')
+            self.assertEqual(row[1], 'test2')
+
+
+if __name__ == '__main__':
+    unittest.main()
