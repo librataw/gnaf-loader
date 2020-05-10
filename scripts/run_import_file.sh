58c498370cbb0c3f488e3544e3d6ac474d538887
William Librata <william.librata@gmail.com>
2020-05-10 19:09:47 +1000

    Fix run_import_file.sh

--- scripts/run_import_file.sh
+++ scripts/run_import_file.sh
@@ -4,17 +4,17 @@ sudo -u postgres psql -d places -f ./create_tables.sql &
 
 while true
 do
-    python3 ../gnaf_load/import.py &
-    python3 ../gnaf_load/import.py &
-    python3 ../gnaf_load/import.py &
-    python3 ../gnaf_load/import.py &
-    python3 ../gnaf_load/import.py &
-    python3 ../gnaf_load/import.py &
-    python3 ../gnaf_load/import.py &
-    python3 ../gnaf_load/import.py &
-    python3 ../gnaf_load/import.py &
-    python3 ../gnaf_load/import.py &
-    python3 ../gnaf_load/import.py &
-    python3 ../gnaf_load/import.py &
+    python3 ../gnaf_loader/import.py &
+    python3 ../gnaf_loader/import.py &
+    python3 ../gnaf_loader/import.py &
+    python3 ../gnaf_loader/import.py &
+    python3 ../gnaf_loader/import.py &
+    python3 ../gnaf_loader/import.py &
+    python3 ../gnaf_loader/import.py &
+    python3 ../gnaf_loader/import.py &
+    python3 ../gnaf_loader/import.py &
+    python3 ../gnaf_loader/import.py &
+    python3 ../gnaf_loader/import.py &
+    python3 ../gnaf_loader/import.py &
     wait
 done
